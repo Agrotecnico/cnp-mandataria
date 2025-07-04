0f309be1f224d03f2e46d84ac692fe1e93b97737
Agrotecnico <agrotecnicog@gmail.com>
2025-07-04 12:09:26 -0300

    Ajuste consulta 7

--- app/lib/brevo/email-respuesta.ts
+++ app/lib/brevo/email-respuesta.ts
@@ -4,7 +4,7 @@ const apiInstance = new brevo.TransactionalEmailsApi();
 
 apiInstance.setApiKey(
     brevo.TransactionalEmailsApiApiKeys.apiKey,
-    process.env.BREVO_API_KEY as string
+    process.env.NEXT_PUBLIC_BREVO_API_KEY as string
 );
 
 interface Params {
